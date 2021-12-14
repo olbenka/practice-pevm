1c0ef162451bba5f9cba87bb578e31ca5d5a0268
olbenka <62645685+olbenka@users.noreply.github.com>
2021-12-14 17:44:33 +0300

    Create 3.cpp

---
+++ misc_cpp/3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+	float x, y, R, r;
+	std::cout << "x, y, R, r" << std::endl;
+	std::cin >> x >> y >> R>> r;
+	int a = x * x + y * y;
+	if ((a < R * R) && (a > r * r))
+	{
+		std::cout << "in";
+	}
+	else {
+		std::cout << "out";
+	}
+    return 0;
+}
