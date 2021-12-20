d499d820488ea4e5ce6b09b6774b9237fcabff1e
olbenka <62645685+olbenka@users.noreply.github.com>
2021-12-20 10:20:14 +0300

    Update 7.cpp

--- arrays/7.cpp
+++ arrays/7.cpp
@@ -1,25 +1,21 @@
 #include <iostream>
-using namespace std;
+
 int main() {
-    int n; // номер комнаты
-    int rooms_in_floor = 1; // кол-во комнат на этаже
-    int t = 0;
-    int rooms = 0; // общее количество комнат
-    int floor = 0; // этаж, на котором будет находиться комната #n
-    cin >> n;
+    int n, room_on_floor = 1, rooms=0, res=0, t=0; 
+    std::cin >> n;
     while (n > 0) {
-        if (rooms_in_floor == t) {
-            t = 0;
-            rooms_in_floor++;
+        if (room_on_floor == t) {
+            room_on_floor++;
         }
-        else if (n - rooms_in_floor > 0) {
-            rooms += rooms_in_floor;
+        else if (n - room_on_floor > 0) {
+            rooms += room_on_floor;
             t++;
-            floor++;
-            n -= rooms_in_floor;
+            res++;
+            n -= room_on_floor;
         }
         else break;
     }
-    cout << floor + 1 << " " << n;
+    std::cout << res + 1 << std::endl; //1 этаж + рез
+    std::cout << n << std::endl;
     return 0;
 }
