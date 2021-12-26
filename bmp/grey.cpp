a5effa8e666412e30af91d8c52a7a1bafcbb8125
olbenka <62645685+olbenka@users.noreply.github.com>
2021-12-27 00:47:15 +0300

    Update grey.cpp

--- bmp/grey.cpp
+++ bmp/grey.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
 #include "BmpStructures.h"
 #include "ImgFunctions.h"
-typedef unsigned long temp;
 
-RgbImg gray(RgbImg const& image)
+RgbImg Gray(RgbImg const& image)
 {
     RgbImg greypic = copyRgbImg(image);
 
-    for (temp row = 0; row < image.height; ++row)
+    for (size_t row = 0; row < image.height; ++row)
    {
-        for (temp col = 0; col < image.width; ++col)
+        for (size_t col = 0; col < image.width; ++col)
         {
             RGB const pixel = image.pixels[row][col];
             unsigned char const gray = (char)((pixel.Blue + pixel.Green + pixel.Red) / 3);
-           greypic.pixels[row][col] = {gray, gray, gray};
+            greypic.pixels[row][col] = {gray, gray, gray};
         }
     }
 
     return greypic;
+}
+
+int main(int argc, char const* argv[])
+{
+    RgbImg input = readRgbImg("RainbowDash.bmp");
+    RgbImg gray = Gray(input);
+
+    writeRgbImg("NoRainbowDash.bmp", gray);
+
+    deleteRgbImg(input);
+    deleteRgbImg(gray);
+
+    return 0;
 }
