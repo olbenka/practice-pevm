47599d045fc1e7f5cffe134cdba0396506c8e507
olbenka <62645685+olbenka@users.noreply.github.com>
2021-12-27 18:01:32 +0300

    Update 5.cpp

--- arrays/5.cpp
+++ arrays/5.cpp
@@ -6,15 +6,25 @@ int main() {
     int* a = new int[n];
     int sum = 0;
     int maxsum = 0;
+    int a1 = 0;
+    int a2 = 0;
     for (int i = 0; i < n; ++i) {
         std::cin >> a[i];
     }
     for (int i = 0; i < n-1; ++i) {
         sum = a[i] + a[i + 1];
-        if (sum > maxsum) maxsum = sum;
-        else sum = 0;
+        if (sum > maxsum) {
+            maxsum = sum;
+            a1 = a[i];
+            a2 = a[i + 1];
+        }
+        else {
+            sum = 0;
+            a1 = 0;
+            a2 = 0;
+        }
     }
-    std::cout << maxsum;
+    std::cout << a1 << " + " << a2 << " = " << maxsum;
     delete[] a;
     return 0;
 }
