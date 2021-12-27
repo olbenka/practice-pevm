aba4d3dbc278e6370b10e1b0ee3375075bc806a2
olbenka <62645685+olbenka@users.noreply.github.com>
2021-12-27 17:50:58 +0300

    Update 4.cpp

--- arrays/4.cpp
+++ arrays/4.cpp
@@ -1,17 +1,23 @@
-#include <iostream>
+#include<iostream>
 
 int main() {
-    int n = 10;
-    int A[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-    for (int i = 0; i < n / 2; i++)
-    {
-        std::cout << A[i] << ", " << A[n - i - 1] << ", ";
+    int n;
+    std::cin >> n;
+    int* a = new int[n];
+    for (int i = 0; i < n; ++i) {
+        std::cin >> a[i];
     }
-    if (n % 2 == 1)
-        std::cout << A[n / 2];
-
-    std::cout << std::endl;
-
+    if (n % 2 == 0) {
+        for (int i = 0; i < n / 2; ++i) {
+            std::cout << a[i] << a[n - 1 - i];
+        }
+    }
+    else {
+        int k = n - n / 2 - 1;
+        for (int i = 0; i < n/2; ++i) {
+            std::cout << a[i] << a[n - 1 - i];
+        }
+        std::cout << a[k];
+    }
     return 0;
 }
